7bb53fb3937c1ff8ccfb134c7014ebbfa6f42ad5
Ayan Ali <149358183+ayanali31990@users.noreply.github.com>
2025-12-04 10:19:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
 
+//your code here
+
+function swapTheme() {
+  const app = document.getElementById("app");
+  const button = document.getElementById("swap");
+
+  // Toggle between day and night
+  if (app.classList.contains("day")) {
+    app.classList.replace("day", "night");
+    button.classList.replace("button_day", "button_night");
+  } else {
+    app.classList.replace("night", "day");
+    button.classList.replace("button_night", "button_day");
+  }
+}
